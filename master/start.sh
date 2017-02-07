6c3414936f8796dce3964e5c3660708087250b7b
longkeyu <longkeyulky@didichuxing.com>
2017-02-07 15:24:53 +0800

    fix

--- master/start.sh
+++ master/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker run --name master --rm \
+-p 10086:3306 \
 -v /Users/didi/docker/mysql/master/conf:/etc/mysql/conf.d \
 -v /Users/didi/docker/mysql/master/data:/var/lib/mysql \
 -e MYSQL_ROOT_PASSWORD=123456 \
